b826f6fa1b73e59311edb1f8403e86060e970242
David <dhutchis68516@gmail.com>
2021-02-11 10:25:15 -0600

    fix typos

--- src/reducers/indexReducer.js
+++ src/reducers/indexReducer.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux';
-import postReducer from './posts';
+import courseReducer from './courseReducer';
 
 const rootReducer = combineReducers({
-  postReducer
+  courseReducer
 });
 
 export default rootReducer
