e05f7cf2788a096fcd313e99322fa8e6951a3fe5
David <dhutchis68516@gmail.com>
2021-02-11 11:21:30 -0600

    set up redux store and initial app

--- src/index.js
+++ src/index.js
@@ -5,9 +5,15 @@ import App from './App';
 import {Provider} from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import rootReducer from './reducers/indexReducer'
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 
 ReactDOM.render(
-    <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
 
