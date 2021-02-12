4af92d2282e15cb2b38d8afa69b27a36f8563187
David <dhutchis68516@gmail.com>
2021-02-12 15:28:05 -0600

    create a list for scores for future use

---
+++ src/components/ScoreList.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { getScores } from '../actions/scores'
+
+class ScoreList extends Component {
+    componentDidMount() {
+        this.props.getScores()
+    }
+    
+    render() {
+        console.log("rendering")
+
+        const scoreLis = this.props.scores.map(s => { 
+        return(
+        <li key={s.id}>{s.round} - {s.adjusted_round}</li>)
+        })
+
+
+        return (
+            <div className="Scorelist">
+                <br/>
+                <h1 className="text-center">My Scores</h1>
+                <br/>
+                <ul>
+                {this.props.loading ? <h3>Loading....</h3> : scoreLis}
+                </ul>
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        scores: state.trailReducer.scores,
+        loading: state.parkReducer.loading
+    }
+}
+
+export default connect(mapStateToProps, { getScores })(ScoreList)
