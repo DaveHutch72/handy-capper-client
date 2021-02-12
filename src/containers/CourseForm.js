93d92243e907019d9d2d4352a4ed62fa20ad2585
David <dhutchis68516@gmail.com>
2021-02-12 09:18:20 -0600

    add state and change content to course form

--- src/containers/CourseForm.js
+++ src/containers/CourseForm.js
@@ -1,6 +1,29 @@
 import React, { Component } from 'react';
 
 class CourseForm extends Component {
+
+    state = {
+        course: {
+            name: '',
+            par: 0,
+            rating: 0,
+            slope: 0
+        },
+        loading: false
+    }
+
+    handleOnChangeContent = (event) => {
+        this.setState({...this.state,
+            course: {...this.state.course,
+                name: event.target.value,
+                par: event.target.value,
+                rating: event.target.value,
+                slope: event.target.value}
+        })
+    }
+
+
+    
     render() {
         return (
             <div>
